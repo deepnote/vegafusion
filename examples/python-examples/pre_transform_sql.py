3c0057015d1795ccec440d58d33bbb9000e8df22
OlegWock <olegwock@gmail.com>
2025-07-14 10:40:07 +0200

    Try with different spec

--- examples/python-examples/pre_transform_sql.py
+++ examples/python-examples/pre_transform_sql.py
@@ -23,12 +23,12 @@ def main():
         'customer_name': pd.Series([], dtype='object'),
         'customer_email': pd.Series([], dtype='object'),
         'customer_age': pd.Series([], dtype='int64'),
-        'country': pd.Series([], dtype='object'),
+        'customer_country': pd.Series([], dtype='object'),
         'customer_segment': pd.Series([], dtype='object'),
         'satisfaction_score': pd.Series([], dtype='int64')
     })
     
-    spec, datasets, warnings = vf.runtime.pre_transform_logical_plan(
+    transformed_spec, datasets, warnings = vf.runtime.pre_transform_logical_plan(
         spec, inline_datasets={"orders": orders_df}
     )
     assert warnings == []
@@ -41,14 +41,13 @@ def main():
 
 
 def get_spec() -> dict[str, Any]:
-    """
-    Based on https://vega.github.io/editor/#/examples/vega/histogram-null-values
-    """
+    # "url": "vegafusion+dataset://orders",
     spec_str = r"""
 {
   "$schema": "https://vega.github.io/schema/vega/v6.json",
   "background": "white",
   "padding": 5,
+  "width": 300,
   "height": 300,
   "style": "cell",
   "data": [
@@ -57,51 +56,70 @@ def get_spec() -> dict[str, Any]:
       "url": "vegafusion+dataset://orders",
       "transform": [
         {
+          "type": "formula",
+          "expr": "toDate(datum[\"datetime\"])",
+          "as": "datetime"
+        },
+        {
+          "field": "datetime",
+          "type": "timeunit",
+          "units": ["year", "month"],
+          "as": ["yearmonth_datetime", "yearmonth_datetime_end"]
+        },
+        {
+          "type": "formula",
+          "expr": "0.5 * timeOffset('month', datum['yearmonth_datetime'], -1) + 0.5 * datum['yearmonth_datetime']",
+          "as": "yearmonth_datetime_offsetted_rect_start"
+        },
+        {
+          "type": "formula",
+          "expr": "0.5 * datum['yearmonth_datetime'] + 0.5 * datum['yearmonth_datetime_end']",
+          "as": "yearmonth_datetime_offsetted_rect_end"
+        },
+        {
           "type": "aggregate",
-          "groupby": ["product_category", "country"],
+          "groupby": [
+            "yearmonth_datetime",
+            "yearmonth_datetime_end",
+            "yearmonth_datetime_offsetted_rect_start",
+            "yearmonth_datetime_offsetted_rect_end"
+          ],
           "ops": ["sum"],
           "fields": ["order_total"],
           "as": ["sum_order_total"]
         },
         {
-          "type": "stack",
-          "groupby": ["product_category"],
-          "field": "sum_order_total",
-          "sort": {"field": ["country"], "order": ["descending"]},
-          "as": ["sum_order_total_start", "sum_order_total_end"],
-          "offset": "zero"
-        },
-        {
           "type": "filter",
-          "expr": "isValid(datum[\"sum_order_total\"]) && isFinite(+datum[\"sum_order_total\"])"
+          "expr": "(isDate(datum[\"yearmonth_datetime\"]) || (isValid(datum[\"yearmonth_datetime\"]) && isFinite(+datum[\"yearmonth_datetime\"]))) && isValid(datum[\"sum_order_total\"]) && isFinite(+datum[\"sum_order_total\"])"
         }
       ]
     }
   ],
-  "signals": [
-    {"name": "x_step", "value": 20},
-    {
-      "name": "width",
-      "update": "bandspace(domain('x').length, 0.1, 0.05) * x_step"
-    }
-  ],
   "marks": [
     {
-      "name": "marks",
+      "name": "layer_0_layer_0_layer_0_marks",
       "type": "rect",
+      "clip": true,
       "style": ["bar"],
       "from": {"data": "data_0"},
       "encode": {
         "update": {
-          "fill": {"scale": "color", "field": "country"},
+          "tooltip": {
+            "signal": "{\"datetime (year-month)\": timeFormat(datum[\"yearmonth_datetime\"], timeUnitSpecifier([\"year\",\"month\"], {\"year-month\":\"%b %Y \",\"year-month-date\":\"%b %d, %Y \"})), \"Sum of order_total\": format(datum[\"sum_order_total\"], \"\")}"
+          },
+          "fill": {"scale": "layer_0_layer_0_color", "value": "order_total"},
           "ariaRoleDescription": {"value": "bar"},
           "description": {
-            "signal": "\"product_category: \" + (isValid(datum[\"product_category\"]) ? datum[\"product_category\"] : \"\"+datum[\"product_category\"]) + \"; Sum of order_total: \" + (format(datum[\"sum_order_total\"], \"\")) + \"; country: \" + (isValid(datum[\"country\"]) ? datum[\"country\"] : \"\"+datum[\"country\"])"
+            "signal": "\"datetime (year-month): \" + (timeFormat(datum[\"yearmonth_datetime\"], timeUnitSpecifier([\"year\",\"month\"], {\"year-month\":\"%b %Y \",\"year-month-date\":\"%b %d, %Y \"}))) + \"; Sum of order_total: \" + (format(datum[\"sum_order_total\"], \"\"))"
           },
-          "x": {"scale": "x", "field": "product_category"},
-          "width": {"signal": "max(0.25, bandwidth('x'))"},
-          "y": {"scale": "y", "field": "sum_order_total_end"},
-          "y2": {"scale": "y", "field": "sum_order_total_start"}
+          "xc": {
+            "scale": "x",
+            "field": "yearmonth_datetime",
+            "offset": {"scale": "xOffset", "value": "series_0", "band": 0.5}
+          },
+          "width": {"signal": "max(0.25, bandwidth('xOffset'))"},
+          "y": {"scale": "y", "field": "sum_order_total"},
+          "y2": {"scale": "y", "value": 0}
         }
       }
     }
@@ -109,28 +127,42 @@ def get_spec() -> dict[str, Any]:
   "scales": [
     {
       "name": "x",
-      "type": "band",
-      "domain": {"data": "data_0", "field": "product_category", "sort": true},
-      "range": {"step": {"signal": "x_step"}},
-      "paddingInner": 0.1,
-      "paddingOuter": 0.05
+      "type": "time",
+      "domain": {
+        "data": "data_0",
+        "fields": [
+          "yearmonth_datetime_offsetted_rect_start",
+          "yearmonth_datetime_offsetted_rect_end"
+        ]
+      },
+      "range": [0, {"signal": "width"}]
     },
     {
       "name": "y",
       "type": "linear",
-      "domain": {
-        "data": "data_0",
-        "fields": ["sum_order_total_start", "sum_order_total_end"]
-      },
+      "domain": {"data": "data_0", "field": "sum_order_total"},
       "range": [{"signal": "height"}, 0],
       "nice": true,
       "zero": true
     },
     {
-      "name": "color",
+      "name": "xOffset",
+      "type": "band",
+      "domain": ["series_0"],
+      "range": [
+        {
+          "signal": "-0.4 * (scale('x', datetime(2001, 1, 1, 0, 0, 0, 0)) - scale('x', datetime(2001, 0, 1, 0, 0, 0, 0)))"
+        },
+        {
+          "signal": "0.4 * (scale('x', datetime(2001, 1, 1, 0, 0, 0, 0)) - scale('x', datetime(2001, 0, 1, 0, 0, 0, 0)))"
+        }
+      ]
+    },
+    {
+      "name": "layer_0_layer_0_color",
       "type": "ordinal",
-      "domain": {"data": "data_0", "field": "country", "sort": true},
-      "range": "category"
+      "domain": ["order_total"],
+      "range": ["#2266D3"]
     }
   ],
   "axes": [
@@ -152,10 +184,16 @@ def get_spec() -> dict[str, Any]:
       "scale": "x",
       "orient": "bottom",
       "grid": false,
-      "title": "product_category",
-      "labelAlign": "right",
-      "labelAngle": 270,
-      "labelBaseline": "middle",
+      "title": "datetime (year-month)",
+      "format": {
+        "signal": "timeUnitSpecifier([\"year\",\"month\"], {\"year-month\":\"%b %Y \",\"year-month-date\":\"%b %d, %Y \"})"
+      },
+      "labelFlush": true,
+      "labelOverlap": true,
+      "tickCount": {"signal": "ceil(width/40)"},
+      "tickMinStep": {
+        "signal": "datetime(2001, 1, 1, 0, 0, 0, 0) - datetime(2001, 0, 1, 0, 0, 0, 0)"
+      },
       "zindex": 0
     },
     {
@@ -168,7 +206,14 @@ def get_spec() -> dict[str, Any]:
       "zindex": 0
     }
   ],
-  "legends": [{"fill": "color", "symbolType": "square", "title": "country"}]
+  "legends": [{"fill": "layer_0_layer_0_color", "symbolType": "square"}],
+  "config": {"legend": {"disable": false}},
+  "usermeta": {
+    "seriesNames": ["order_total"],
+    "seriesOrder": [0],
+    "specSchemaVersion": 2,
+    "tooltipDefaultMode": true
+  }
 }
 """
     return json.loads(spec_str)
